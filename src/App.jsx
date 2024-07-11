8dab1a8d90b2b3c325759aec9058088dbe3bba89
srhqmp <ballanosarah@gmail.com>
2024-07-12 00:04:14 +0800

    feat: display whois data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,60 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import whois from "./services/whois";
+
+const DomainInformation = ({ domain }) => {
+  console.log({ domain });
+  return (
+    <div>
+      <h3>Domain Information</h3>
+      <table>
+        <thead>
+          <th>Domain Name</th>
+          <th>Registrar</th>
+          <th>Registration Date</th>
+          <th>Expiration Date</th>
+          <th>Estimated Domain Age</th>
+          <th>Hostnames</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{domain.domainName}</td>
+            <td>{domain.registrarName}</td>
+            <td>{domain.createdDate}</td>
+            <td>{domain.expiresDate}</td>
+            <td>{domain.estimatedDomainAge}</td>
+            <td>
+              {domain.nameServers.hostNames.join(", ").slice(0, 25 - 3) + "..."}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const ContactInformation = ({ domain }) => {
+  return (
+    <div>
+      <h3>Contact Information</h3>
+      <table>
+        <thead>
+          <th>Registrant Name</th>
+          <th>Technical Contact Name</th>
+          <th>Administrative Contact Name</th>
+          <th>Contact Email</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{domain.registrant.name}</td>
+            <td>{domain.technicalContact.name}</td>
+            <td>{domain.administrativeContact.name}</td>
+            <td>{domain.contactEmail}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 const App = () => {
   const [search, setSearch] = useState("");
@@ -36,6 +91,12 @@ const App = () => {
         <button type="submit">search</button>
       </form>
       {errorMessage && <div>{errorMessage}</div>}
+      {domain && (
+        <>
+          <DomainInformation domain={domain.WhoisRecord} />
+          <ContactInformation domain={domain.WhoisRecord} />
+        </>
+      )}
     </div>
   );
 };
