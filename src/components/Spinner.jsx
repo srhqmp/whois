ff2cfdc756adf85c45f429d5893d1d85ca4d0d3c
srhqmp <ballanosarah@gmail.com>
2024-07-12 13:03:39 +0800

    style: spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,8 +1,10 @@
 const Spinner = () => {
   return (
     <div className="fixed flex-col gap-2 top-0 left-0 w-full h-full flex items-center justify-center">
-      <div className="animate-spin rounded-full h-16 w-16  border-b-4 border-secondary-100"></div>
-      <div>Loading...</div>
+      <div className="bg-white px-4 py-6 rounded-lg flex items-center justify-center flex-col gap-2">
+        <div className="animate-spin rounded-full h-12 w-12 md:h-16 md:w-16 border-b-4 border-secondary-100"></div>
+        <div>Loading...</div>
+      </div>
     </div>
   );
 };
